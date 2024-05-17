510dccb3491814f58253431436b02c4ec7aee13d
David Díaz <vidda.redeyes@gmail.com>
2024-05-16 20:17:05 -0400

    inventario mejorado

--- JS/agregar_inv.js
+++ JS/agregar_inv.js
@@ -1,40 +1,34 @@
-var boton = document.getElementById('agregar');
+var boton = document.getElementById('agregar_prod');
 var guardar = document.getElementById('guardar');
 var borraar = document.getElementById('codigo_emp');
-var lista = document.getElementById('lista_emp');
+var lista = document.getElementById('lista_prod');
 var data = [];
 var cant = 1;
-boton.addEventListener("click",agregarEmp);
+boton.addEventListener("click",agregarProd);
 guardar.addEventListener("click",save);
 
-function agregarEmp(){
-    var nombre = document.getElementById('nombre_emp').value;
-    var rut = parseFloat(document.getElementById('rut_emp').value);
-    var dv = document.getElementById('dv_emp').value;
-    var sueldo = parseFloat(document.getElementById('sueldo_emp').value);
-    var email = document.getElementById('email_emp').value;
+function agregarProd(){
+    var nombre_prod = document.getElementById('nombre_prod').value;
+    var cantidad = parseFloat(document.getElementById('cantidad_prod').value);
+    var formato_alm = document.getElementById('formato_prod').value;
 
     data.push({
             "id" : cant,
-            "nombre" : nombre,
-            "rut" : rut,
-            "dv" : dv,
-            "sueldo" : sueldo,
-            "email" : email
+            "nombre" : nombre_prod,
+            "cantidad" : cantidad,
+            "formato" : formato_alm
             }
     );
     var col_id = 'row' + cant;
-    var fila = '<tr id =' + col_id +'><td>'+cant+'</td><td>'+nombre+'</td><td>'+rut+'</td><td>'+dv+'</td><td>'+sueldo+'</td><td>'+email+
+    var fila = '<tr id =' + col_id +'><td>'+cant+'</td><td>'+nombre_prod+'</td><td>'+cantidad+'</td><td>'+formato_alm+
     '</td><td><a href = "#" class = "btn btn-danger" onclick= "eliminar('+cant+')";>Eliminar</a></td></tr>'
     console.log(fila);
-    $('#lista_emp').append(fila);
-    $('#id_emp').val('');
-    $('#nombre_emp').val('');
-    $('#rut_emp').val('');
-    $('#dv_emp').val('');
-    $('#sueldo_emp').val('');
-    $('#email_emp').val('');
-    $('#nombre_emp').focus();
+    $('#lista_prod').append(fila);
+    $('#id_prod').val('');
+    $('#nombre_prod').val('');
+    $('#cantidad_prod').val('');
+    $('#formato_prod').val('');
+    $('#nombre_prod').focus();
     cant++;
 };
 function save(){
